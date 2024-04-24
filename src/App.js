096a0473a2bcefb1a7c1b7e1797307aeebac1fff
reuht <jhosephvillalba@gmail.com>
2024-04-24 08:30:56 -0500

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import Lienal from './Components/Lineal';
+import Sector from './Components/sector';
+import Bars from './Components/Bars';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className="container-fluid">
+      <div className='col'>
+        <div className='row'>
+          <div className='col-9 p-2'>
+            <Lienal/>
+          </div>
+          <div className='col-3'>
+            <h1>El nuevo</h1>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col-3 p-3'> 
+            <h1>Grafico 1</h1>
+          </div>
+          <div className='col-3 p-3'> 
+          <h1>Grafico 1</h1>
+          </div>
+          <div className='col-3 p-3'> 
+              <Sector />
+          </div>
+          <div className='col-3 p-3'> 
+            <div className='d-flex'>
+              <div className='title'>
+              Proyecto por constructora
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Bars.js
@@ -0,0 +1,75 @@
+import React, {Component} from "react";
+import ReactApexChart from "react-apexcharts";
+
+
+class Bars extends Component {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+      
+        series: [{
+          data: [21, 22, 10, 28, 16, 21, 13, 30]
+        }],
+        options: {
+          chart: {
+            height: 350,
+            type: 'bar',
+            events: {
+              click: function(chart, w, e) {
+                // console.log(chart, w, e)
+              }
+            }
+          },
+         
+          plotOptions: {
+            bar: {
+              columnWidth: '45%',
+              distributed: true,
+            }
+          },
+          dataLabels: {
+            enabled: false
+          },
+          legend: {
+            show: false
+          },
+          xaxis: {
+            categories: [
+              ['John', 'Doe'],
+              ['Joe', 'Smith'],
+              ['Jake', 'Williams'],
+              'Amber',
+              ['Peter', 'Brown'],
+              ['Mary', 'Evans'],
+              ['David', 'Wilson'],
+              ['Lily', 'Roberts'], 
+            ],
+            labels: {
+              style: {
+                
+                fontSize: '12px'
+              }
+            }
+          }
+        },
+      
+      
+      };
+    }
+
+  
+
+    render() {
+      return (
+        <div>
+          <div id="chart">
+            <ReactApexChart options={this.state.options} series={this.state.series} type="bar" height={350} />
+          </div>
+          <div id="html-dist"></div>
+        </div>
+      );
+    }
+  }
+
+  export default Bars; 
