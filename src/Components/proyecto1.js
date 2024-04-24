8033973dd28a9247d9608930093ff26b5825a282
nose <email@email.com>
2024-04-24 17:24:18 -0500

    componentes terminados

---
+++ src/Components/proyecto1.js
@@ -0,0 +1,43 @@
+// Proyecto1.js
+import React from 'react';
+
+const Proyecto1 = ({ title, info, total }) => {
+  return (
+    <div className="container">
+      <div className="title-container">
+        <h2 className="title">{title}</h2>
+        <div className="subtitle">Constructora</div>
+      </div>
+      <div className="total">
+        <p>{total}</p>
+      </div>
+      
+      <div className="wrapper small-scroll"> {/* Aplica la clase para el scroll */}
+        {info.length > 0 ? (
+          <table className="table">
+            <thead>
+              <tr>
+                <th></th>
+                <th>Total</th>
+              </tr>
+            </thead>
+            <tbody>
+              {info.map((item, index) => (
+                <tr key={index}>
+                  <td>{item.texto}</td>
+                  <td>{item.total}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <p>No hay información disponible</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Proyecto1;
+
+
